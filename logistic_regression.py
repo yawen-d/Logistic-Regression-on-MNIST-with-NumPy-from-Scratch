2c84088506c438526b8e5874a56c6f066b50cc90
kmdanielduan <kmdanielduan@gmail.com>
2019-05-18 10:00:52 +0800

    update the main program

--- logistic_regression.py
+++ logistic_regression.py
@@ -5,23 +5,24 @@ import time
 import copy
 from random import randint
 
-# cd Desktop/CS\ 398/Assignments/A1/
-#load MNIST data
-MNIST_data = h5py.File('MNISTdata.hdf5', 'r')
-x_train = np.float32(MNIST_data['x_train'][:])
-y_train = np.int32(np.array(MNIST_data['y_train'][:,0]))
-x_test = np.float32(MNIST_data['x_test'][:])
-y_test = np.int32(np.array(MNIST_data['y_test'][:,0]))
-MNIST_data.close()
+# load MNIST data
+def load_mnist(filename):
+    """
+    load the mnist data
 
-####################################################################################
-#Implementation of stochastic gradient descent algorithm
-#number of inputs
-num_inputs = 28*28
-#number of outputs
-num_outputs = 10
+    Arguments:
+    filename -- the filename string
 
-
+    Return:
+    x_train(train_data),y_train(train_label),x_test(test_data),y_test(test_label)
+    """
+    MNIST_data = h5py.File(filename, 'r')
+    x_train = np.float32(MNIST_data['x_train'][:])
+    y_train = np.int32(np.array(MNIST_data['y_train'][:,0]))
+    x_test = np.float32(MNIST_data['x_test'][:])
+    y_test = np.int32(np.array(MNIST_data['y_test'][:,0]))
+    MNIST_data.close()
+    return x_train,y_train,x_test,y_test
 
 def softmax(z):
     """
@@ -34,7 +35,8 @@ def softmax(z):
     result -- the softmax function evaluated on z, returning a set of probability 
     of length k
     """
-    result = 1/sum(np.exp(z)) * np.exp(z)
+    exp_list = np.exp(z)
+    result = 1/sum(exp_list) * exp_list
     assert (result.shape == (len(z),))
     return result
 
@@ -52,7 +54,9 @@ def gradient(w, x, y):
     """
     E = np.array([0]*10).reshape((1,10))
     E[0][y] = 1
-    A = (E - softmax(np.matmul(w, x))).reshape((10,1))
+    print(w.shape)
+    print(x.shape)
+    A = (E - softmax(np.matmul(x, w))).reshape((10,1))
     grad = np.squeeze(x[0]*A)
 
     for i in range(1,len(x)):
@@ -70,13 +74,13 @@ def initialize(num_outputs,num_inputs):
     """
     return np.random.randn(num_outputs,num_inputs) / np.sqrt(num_inputs)
 
-def model (X_train, Y_train, num_iterations = 1000, learning_rate = 0.5):
+def model (x_train, y_train, num_iterations = 1000, learning_rate = 0.5):
     """
-    Implement the stochastic gradient descent model 
+    Implement the logistic regression model with stochastic gradient descent 
 
     Arguments:
-    X_train -- x_train data set, a 2darray of float in shape (num_training_data, num_inputs)
-    Y_train -- y_train data set, a vector of float in shape (num_training_data,)
+    x_train -- x_train data set, a 2darray of float in shape (num_training_data, num_inputs)
+    y_train -- y_train data set, a vector of float in shape (num_training_data,)
     num_iterations -- number of iterations to have
     learning_rate -- size of base learning rate
 
@@ -84,10 +88,13 @@ def model (X_train, Y_train, num_iterations = 1000, learning_rate = 0.5):
     w -- the weights after optimization, an 2darray of size (num_outputs, num_inputs)
     """
     # initialize the random weights
-    w = initialize(num_outputs,num_inputs)
+    num_inputs = x_train.shape[1]
+    num_class = len(set(y_train))
+    w = initialize(num_inputs,num_class)
+
     # generate a random list of indices for the training set
-    train_size = len(x_train)
-    rand_indices = np.random.choice(train_size, num_iterations, replace=True)
+    train_data_size = x_train.shape[0]
+    rand_indices = np.random.choice(train_data_size, num_iterations, replace=True)
 
     def l_rate(base_rate, ite, num_iterations, schedule = False):
         if schedule == True:
@@ -96,7 +103,7 @@ def model (X_train, Y_train, num_iterations = 1000, learning_rate = 0.5):
             return base_rate
 
     for i in rand_indices:
-        w = w - gradient(w,X_train[i],Y_train[i]) * l_rate(learning_rate, i, num_iterations)
+        w = w - gradient(w,x_train[i],y_train[i]) * l_rate(learning_rate, i, num_iterations)
     return w 
 
 def predict (w, x):
@@ -113,35 +120,38 @@ def predict (w, x):
     result = np.argmax(dist)
     return result
 
-def testing(w, X_test, Y_test):
+def testing(w, x_test, y_test):
     """
     Test the model 
 
     Arguments:
     w -- the weights after optimization, an 2darray of size (num_outputs, num_inputs)
-    X_test -- x_test data set, a 2darray of float in shape (num_testing_data, num_inputs)
-    Y_test -- y_test data set, a vector of float in shape (num_testing_data,)
+    x_test -- x_test data set, a 2darray of float in shape (num_testing_data, num_inputs)
+    y_test -- y_test data set, a vector of float in shape (num_testing_data,)
     """
     total_correct = 0
-    for n in range(len(X_test)):
-        y = Y_test[n]
-        x = X_test[n][:]
+    for n in range(len(x_test)):
+        y = y_test[n]
+        x = x_test[n][:]
         prediction = predict (w , x)
         if (prediction == y):
             total_correct += 1
     # print('Accuarcy Test: ',total_correct/np.float(len(X_test)))
-    return total_correct/np.float(len(X_test))
-
-for l in [0.01,0.02,0.03]:
-    for n in [8000,10000,12000]:
-        w = model(x_train, y_train, num_iterations = n, learning_rate = l)
-        print('Model #', (n,l))
-        print('Number of iterations =',n)
-        print('Learning Rate =',l)
-        print('Accuarcy Test: ',testing(w,x_test,y_test))
-        print('################################')
-
-######################################################################################
-# test data
+    return total_correct/np.float(len(x_test))
 
 
+def main():
+    x_train,y_train,x_test,y_test = load_mnist('MNISTdata.hdf5')
+
+    for l in [0.02]:
+        for n in [10000]:
+            w = model(x_train, y_train, num_iterations = n, learning_rate = l)
+            print('Model #', (n,l))
+            print('Number of iterations =',n)
+            print('Learning Rate =',l)
+            print('Accuarcy Test: ',testing(w,x_test,y_test))
+            print('################################')
+
+
+if __name__ == "__main__":
+    main()
